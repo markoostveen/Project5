eb469474eb2f143a698b3afadcc0fdd1d6d4a0cc
Alienware 15R3 <markoostveen1@gmail.com>
2017-09-25 08:56:26 +0200

    Started Working on Pickups

---
+++ Periode 5/Assets/Script/Pickup/Pickup.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Collider))]
+public class Pickup : MonoBehaviour {
+
+    private Collider m_Collider;
+
+    private void Start()
+    {
+        m_Collider = GetComponent<Collider>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+            other.SendMessage("PickUp");
+    }
+}
