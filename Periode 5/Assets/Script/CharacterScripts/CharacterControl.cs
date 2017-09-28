6641ed3260e086938f1ea2b6e66437fa1dae8827
Fabio van Haarlem <fabio.van.haarlem@hotmail.nl>
2017-09-28 12:23:48 +0200

    Changed prefabs

--- Periode 5/Assets/Script/CharacterScripts/CharacterControl.cs
+++ Periode 5/Assets/Script/CharacterScripts/CharacterControl.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using System;
 
-public class CharacterControl : PoolObject
+public class CharacterControl : MonoBehaviour
 {
     private Walking m_WalkingState;
     private Fishing m_FishingState;
